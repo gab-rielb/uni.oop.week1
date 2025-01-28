1813d1bb12e78bec8e1834a307baaf0fbe2cf36e
Gabriel Berezin <gabrielberezin123@gmail.com>
2025-01-28 17:37:50 +0000

    Task 4 - Compute and display a histogram

--- Program.cs
+++ Program.cs
@@ -26,6 +26,10 @@ namespace lecture1
             int array_sum = ComputeSum(random_num_array);
             Console.WriteLine("\n\nSum: " + array_sum);
 
+            // Task 4 - Compute and display the histogram
+            int[,] histogram = ComputeHistogram(random_num_array);
+            DisplayHistogram(histogram, random_num_array);
+
         }
 
         // T2.1 - Implement a method called GenerateRandomInteger
@@ -54,6 +58,34 @@ namespace lecture1
             }
             
             return sum;
+        }
+
+        // T4.1 - Write a method called ComputeHistogram that
+        // computes the frequency of each number and stores it in an array.
+        static int[,] ComputeHistogram(int[] generated_array)
+        {
+            int unique_num_count = generated_array.Distinct().Count(); // Gets the number of unique numbers
+            int[,] histogram = new int[unique_num_count, 1]; // Generates a 2D array for each unique number (number, frequency)
+            
+
+            foreach (int num in generated_array)
+            {
+                int index = Array.IndexOf(generated_array.Distinct().ToArray(), num); // Finds the correct index to increment
+                histogram[index, 0]++; // Increments the frequency
+            }
+
+            return histogram;
+        }
+
+        // T4.2 - Implement a method named DisplayHistogram to print
+        // out the frequency of each number.
+        static void DisplayHistogram(int[,] histogram, int[] generated_array)
+        {
+            Console.WriteLine("\n\nHistogram:");
+            for (int i = 0; i < histogram.GetLength(0); i++)
+            {
+                Console.WriteLine($"Number: {generated_array.Distinct().ToArray()[i]}, Frequency: {histogram[i, 0]}");
+            }
         }
     }
 }
